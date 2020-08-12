c21c1725de4b208eb25525b659f47bee6bd91b89
Vladimir Ciobanu <admin@cvlad.info>
2020-08-13 00:26:22 +0300

    Add purescript shell.

--- purescript.nix
+++ purescript.nix
@@ -1,12 +1,22 @@
 { pkgs, easy-ps }:
 let
-  self = {
+  self = rec {
     # for npx to install purescript-language-server
     nodejs = pkgs.nodejs;
     purs = easy-ps.purs;
     spago = easy-ps.spago;
     pscid = easy-ps.pscid;
     spago2nix = easy-ps.spago2nix;
+
+    mkShell = pkgs.mkShell {
+      buildInputs = [
+        self.nodejs
+        self.purs
+        self.spago
+        self.pscid
+        self.spago2nix
+      ];
+    };
   };
 
 in
