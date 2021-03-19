450f7e0b8713586fbce5ce6df3250258aedf38cd
Vladimir Ciobanu <admin@cvlad.info>
2021-03-19 13:35:33 +0200

    export more ghcs

--- haskell/default.nix
+++ haskell/default.nix
@@ -1,9 +1,11 @@
 { pkgs }:
 let
+  ghc901  = import ./ghc-901.nix  { inherit pkgs; };
+  ghc8104 = import ./ghc-8104.nix { inherit pkgs; };
   ghc8102 = import ./ghc-8102.nix { inherit pkgs; };
-  ghc884 = import ./ghc-884.nix { inherit pkgs; };
-  ghc865 = import ./ghc-865.nix { inherit pkgs; };
+  ghc884  = import ./ghc-884.nix  { inherit pkgs; };
+  ghc865  = import ./ghc-865.nix  { inherit pkgs; };
 in
   {
-    inherit ghc8102 ghc884 ghc865;
+    inherit ghc901 ghc8104 ghc8102 ghc884 ghc865;
   }
